2466e1d9180ff81900b5ab9edc9ff930efacd464
praveen srivastava <praveen034u@gmail.com>
2025-05-06 15:19:45 -0400

    updated

--- Virtual-health-ui/wwwroot/auth.js
+++ Virtual-health-ui/wwwroot/auth.js
@@ -1,32 +1,36 @@
-﻿window.showAuth0Login = () => {
-    var lock = new Auth0Lock('V9Una2guriD6USXmVHCn4LVRZX9Ao479', 'dev-jbrriuc5vyjmiwtx.us.auth0.com', {
-        auth: {
-            redirect: false,
-            responseType: 'token id_token',
-            audience: 'https://dev-jbrriuc5vyjmiwtx.us.auth0.com/userinfo',
-            scope: 'openid profile email'
-        }
-    });
+window.showAuth0Login = () => {
+    const container = document.getElementById("auth0-login-container");
 
-    lock.on("authenticated", function (authResult) {
-        lock.getUserInfo(authResult.accessToken, function (error, profile) {
-            if (error) {
-                console.log("Error fetching profile:", error);
-                return;
+    if (container && !container.hasChildNodes()) {
+        var lock = new Auth0Lock('V9Una2guriD6USXmVHCn4LVRZX9Ao479', 'dev-jbrriuc5vyjmiwtx.us.auth0.com', {
+            auth: {
+                redirect: false,
+                responseType: 'token id_token',
+                audience: 'https://dev-jbrriuc5vyjmiwtx.us.auth0.com/userinfo',
+                scope: 'openid profile email'
             }
-            sessionStorage.setItem("access_token", authResult.accessToken);
-            sessionStorage.setItem("id_token", authResult.idToken);
-            sessionStorage.setItem("user_profile", JSON.stringify(profile));
-
-            console.log("Auth success, invoking Blazor method...");
-
-            DotNet.invokeMethodAsync("VirtualHealth.UI", "OnAuthSuccess")
-                .then(() => console.log("Blazor method invoked successfully!"))
-                .catch(err => console.error("Blazor invoke error:", err));
         });
-    });
 
-    lock.show();
+        lock.on("authenticated", function (authResult) {
+            lock.getUserInfo(authResult.accessToken, function (error, profile) {
+                if (error) {
+                    console.log("Error fetching profile:", error);
+                    return;
+                }
+
+                sessionStorage.setItem("access_token", authResult.accessToken);
+                sessionStorage.setItem("id_token", authResult.idToken);
+                sessionStorage.setItem("user_profile", JSON.stringify(profile));
+
+                console.log("Auth success, invoking Blazor method...");
+
+                DotNet.invokeMethodAsync("VirtualHealth.UI", "OnAuthSuccess")
+                    .catch(err => console.error("Blazor callback failed", err));
+            });
+        });
+
+        lock.show();
+    }
 };
 
 window.logoutAuth0 = (logoutUrl) => {
