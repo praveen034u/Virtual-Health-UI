78c428bdaaedb13fd03797626b1dddd8ce8f4c43
praveen srivastava <praveen034u@gmail.com>
2025-06-28 00:42:24 -0400

    updated

--- Virtual-health-ui/Program.cs
+++ Virtual-health-ui/Program.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using System.Text.Json;
 using VirtualHealth.UI;
 using VirtualHealth.UI.Models;
 using VirtualHealth.UI.Services;
